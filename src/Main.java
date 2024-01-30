ef4608e915f3379ce6d82b9f01464cc800843bc2
Anna Evseykina <anytta9032@yandex.ru>
2024-01-30 23:54:10 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 26536;
+        int bonus = 20;
+
+        System.out.println((amount / bonus) + "начислено миль");
+
+    }
+
+
+}
